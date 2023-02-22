d65a82beab13d844ce7cf194d366923cb9f183d0
Rakib <Alvin15148@gmail.com>
2023-02-22 23:31:15 +0100

    test__

---
+++ src/KnightPacking.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class KnightPacking {
+    public static void main(String [] args){
+        int num;
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
+        if(num % 2 == 0){
+            System.out.println("Second");
+        }
+        else{
+            System.out.println("First");
+        }
+
+
+    }
+}
