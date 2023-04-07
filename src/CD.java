f249159a069bad3fb540f0e071472f9596af0091
Rakib <Alvin15148@gmail.com>
2023-04-08 01:44:49 +0200

    ttt

--- src/CD.java
+++ src/CD.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
 
 
 public class CD {
     public static void main(String[]args) throws IOException {
         BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
         String line = scan.readLine();
-
 
         while (!line.equals("0 0")) {
             String[] parts = line.split(" ");
@@ -19,18 +16,15 @@ public class CD {
             int duplicates = 0;
             HashSet<String> cds = new HashSet<String>();
 
-
             for (int i = 0; i < jack; i++) {
                 cds.add(scan.readLine());
             }
-
 
             for (int j = 0; j < jill; j++) {
                 if (cds.contains(scan.readLine())) {
                     duplicates++;
                 }
             }
-
 
             System.out.println(duplicates);
             line = scan.readLine();
